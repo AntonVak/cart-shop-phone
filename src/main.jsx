e5548d5d1d5154c1496995025d6a7ce55acaad54
AntonVak <antonv.snab@gmail.com>
2023-10-09 21:34:33 +0300

    install dependency react-toastify

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,22 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider} from "react-router-dom";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
 import router from "./components/routes/router.jsx";
+import store from "./store/store";
 import theme from "./styles/breakPoints.js";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <ToastContainer />
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
