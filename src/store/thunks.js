dba7968535f206d2d72883b507514019e5357754
AntonVak <antonv.snab@gmail.com>
2023-10-09 19:43:11 +0300

    editing baseUrl & query

--- src/store/thunks.js
+++ src/store/thunks.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const getProducts = createAsyncThunk(
-  "products/getProducts",
+  "home/getProducts",
   async (_, {rejectWithValue} ) => {
     try{
         const response = await axios.get("http://localhost:5000/home");
