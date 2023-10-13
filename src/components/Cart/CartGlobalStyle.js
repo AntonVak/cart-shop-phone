533d34e8817f71831b323a0fb8d62cd19dac16de
AntonVak <antonv.snab@gmail.com>
2023-10-13 15:06:01 +0300

    add styles in CartItems

--- src/components/Cart/CartGlobalStyle.js
+++ src/components/Cart/CartGlobalStyle.js
@@ -1,7 +1,7 @@
 export const styles = {
   section: { paddingTop: "20px", paddingBottom: "20px" },
   title: { textAlign: "center" },
-  row: { bgcolor: "background.headerRow" },
+  row: {  },
 
   headFirstCell: {
     fontSize: "1.8rem",
@@ -10,7 +10,14 @@ export const styles = {
     color: (theme) => ({
       color: theme.status.warning,
     }),
+    textTransform: 'uppercase',
   },
 
-  headCell: { fontSize: "1.8rem", fontWeight: "600", textAlign: "center" },
+  headCell: { fontSize: "1.8rem", fontWeight: "600", textAlign: "center", textTransform: 'uppercase', },
+  card: {},
+  img: {height: '180px', width: '100px'},
+  nameTitle: {fontWeight: 600},
+  descTitle: {fontSize: '1.5rem', paddingBottom: '2rem'},
+  price:{ display: 'block' },
+  total:{}
 };
--- src/components/Cart/CartItems.jsx
+++ src/components/Cart/CartItems.jsx
@@ -1,20 +1,51 @@
 import { Box, Button, Card, CardMedia, Typography } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { styles } from "./CartGlobalStyle";
+import CartQuantity from "./CartQuantity";
+import { useDispatch } from "react-redux";
+import { removeFromCart } from "../../store/cart/cartSlice";
 
-const CartItems = () => {
+const CartItems = ({ item }) => {
+  const { id, name, desc, price, image } = item;
+
+  const dispatchFunc = useDispatch();
+
+  const handlerDelete = () => {
+    dispatchFunc(removeFromCart(id));
+  };
+
   return (
-    <Card sx={{ width: "100%", mt: 5, p: 2 }}>
-      <Box sx={{ display: "flex" }}>
-        <CardMedia component="img" height="100" image="" alt="" />
-        <Typography component="p">Live From Space</Typography>
-
-        <Button size="small" variant="outlined" startIcon={<DeleteIcon />}>
+    <Card sx={{ width: "100%", p: 2, display: "flex" }}>
+      <CardMedia component="img" sx={styles.img} image={image} alt={name} />
+      <Box sx={{ pl: 4, width: "32rem" }}>
+        <Typography component="div" sx={styles.nameTitle}>
+          {name}
+        </Typography>
+        <Typography component="p" sx={styles.descTitle}>
+          {desc}
+        </Typography>
+        <Button
+          size="small"
+          variant="outlined"
+          startIcon={<DeleteIcon />}
+          onClick={handlerDelete}
+        >
           Delete
         </Button>
       </Box>
-      <Box></Box>
+
+      <Box sx={{ display: "flex" }}>
+        <Typography component="div" sx={styles.price}>
+          ${price}
+        </Typography>
+        <CartQuantity />
+        <Typography component="div">Total</Typography>
+      </Box>
     </Card>
   );
 };
+// CartItems.propTypes = {
+//   name: PropTypes.string.isRequired
+// }
 
 export default CartItems;
