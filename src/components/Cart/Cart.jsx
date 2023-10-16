2bb60b2731b01241c3eb7eecba799c7f9ca0960e
AntonVak <antonv.snab@gmail.com>
2023-10-16 19:20:27 +0300

    add dispatch totalPrice

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,7 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useCart } from "../../hooks/useCart";
+import { setTotalPrice } from "../../store/cart/cartSlice";
 import CartFooter from "./CartFooter";
 
 import CartItems from "./CartItems";
@@ -6,16 +9,38 @@ import CartTable from "./CartTable";
 
 const Cart = () => {
   const { items } = useCart();
-  console.log(items);
+  const dispatchFunc = useDispatch();
+
+  useEffect(() => {
+    if(items.length > 0) {
+      const currentTotalPrice = items.reduce((acc, curr) => {
+        return acc + curr.price * curr.cartQuantity
+      },0)
+      dispatchFunc(setTotalPrice(currentTotalPrice));
+    }
+    if (items.length === 0) {
+      dispatchFunc(setTotalPrice(0));
+    }
+  }, [dispatchFunc, items]);
+ 
+
+
+  
   return (
     <>
-      {items.length === 0 ? <p>Yors Cart Empty</p> : <CartTable />}
-      {items.map((item) => (
-        <div key={item.id}>
-          <CartItems item={item} />
-        </div>
-      ))}
-      <CartFooter items/>
+      {items.length === 0 ? (
+        <p>Yors Cart Empty</p>
+      ) : (
+        <>
+          <CartTable />
+          {items.map((item) => (
+            <div key={item.id}>
+              <CartItems item={item} />
+            </div>
+          ))}
+          <CartFooter items={items} />
+        </>
+      )}
     </>
   );
 };
